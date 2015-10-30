46e7792f06b07868bc0213dbeaf6c263d9250c08
Aaron Welch <welch@packet.net>
2015-10-30 09:42:27 -0400

    add some more setups, add display_control init service file

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ wget -O - -q https://apt.adafruit.com/apt.adafruit.com.gpg.key | sudo apt-key ad
 sudo aptitude update
 sudo aptitude purge wolfram-engine
 sudo aptitude upgrade
-sudo aptitude -y install lirc node chromium x11-xserver-utils unclutter
+sudo aptitude -y install lirc node x11-xserver-utils unclutter monit midori ntp
 
 # setup gpio pins and lirc
 echo "dtoverlay=lirc-rpi,gpio_in_pin=27,gpio_out_pin=17" | sudo tee --append /boot/config.txt
@@ -19,7 +19,9 @@ sudo cat BN59-01199F.conf > /etc/lirc/lircd.conf
 sudo cat hardware.conf > /etc/lirc/hardware.conf
 sudo cp monitor.sh /bin
 echo "0 8 * * 1,2,3,4,5 root /bin/monitor.sh on >> /dev/null" | sudo tee --append /etc/crontab
+echo "45 7 * * 1,2,3,4,5 root /etc/init.d/lightdm start >> /dev/null"  | sudo tee --append /etc/crontab
 echo "0 19 * * 1,2,3,4,5 root /bin/monitor.sh off >> /dev/null" | sudo tee --append /etc/crontab
+echo "15 19 * * 1,2,3,4,5 root /etc/init.d/lightdm stop >> /dev/null"  | sudo tee --append /etc/crontab
 
 # setup monitor power settings and kiosk browser
 # from https://www.danpurdy.co.uk/web-development/raspberry-pi-kiosk-screen-tutorial/
@@ -29,7 +31,12 @@ echo '@xset s -dpms' | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
 echo '@xset s noblank' | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
 echo "@sed -i 's/\"exited_cleanly\": false/\"exited_cleanly\": true/' ~/.config/chromium/Default/Preferences" | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
 echo '@rm ~/.config/chromium/SingletonLock' | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
-echo '@chromium --noerrdialogs --kiosk http://status.packet.net --incognito' | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
+#echo '@chromium --noerrdialogs --kiosk http://status.packet.net --incognito' | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
+echo '@midori -i 120 -e Fullscreen -p -a http://status.packet.net' | sudo tee --append /etc/xdg/lxsession/LXDE-pi/autostart
+
+# install display_control service file
+cp display_control /etc/init.d
+ln -s ../init.d/display_control /etc/rc2.d/S01display_control
 
 # move node.js app over
 rsync -avz node ~/
